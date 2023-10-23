a71f0db23efaf16f3d1e8b3f642db3b33d795c18
AlexF87 <feldman87@yandex.ru>
2023-10-23 19:58:33 +0700

    feat: Добавить новый класс BubbleSort

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] arr = new int[random.nextInt(100)];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.floor(Math.random()*100) + 1);
+
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
